59c02daabd2ab1dc51d1da6bfde73453619f8610
Kevin Thomas <kevinthomastm2002@gmail.com>
2023-04-19 01:56:47 +0530

    Initial Commit

--- app/src/main/java/com/kvvin/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/kvvin/mymessenger/CreateMessageActivity.java
@@ -1,6 +1,7 @@
 package com.kvvin.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +14,8 @@ public class CreateMessageActivity extends Activity {
     }
 
     public void onSendMessage(View view){
+        Intent intent = new Intent(this, RecieveMessageActivity.class);
+        startActivity(intent);
 
     }
 }
